463a9a9bf847e9e9c7b51490757fcc024870200e
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-04-14 18:05:34 +0900

    [추가] 랜덤한 세자리 수 생성 기능

--- src/main/java/logic/Player.java
+++ src/main/java/logic/Player.java
@@ -2,19 +2,22 @@ package logic;
 
 import domain.GameNumber;
 import util.InputView;
+import util.RandomNumberMaker;
 
 public class Player {
 
+    private static final int GAME_COUNT = 3;
     private GameNumber playerGameNumber;
+    private GameNumber randomGameNumber;
 
     public Player() {
         init();
     }
     private void init() {
+        randomGameNumber = RandomNumberMaker.getRandomGameNumber(GAME_COUNT);
         playerGameNumber = InputView.getGameNumber();
     }
 
     public void play() {
-        System.out.println(playerGameNumber.getGameNumbers());
     }
 }
---
+++ src/main/java/util/RandomNumberMaker.java
@@ -0,0 +1,32 @@
+package util;
+
+import domain.GameNumber;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class RandomNumberMaker {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+
+    public static GameNumber getRandomGameNumber(int count) {
+        try {
+            return new GameNumber(getGameNumberString(count));
+        } catch (IllegalArgumentException e) {
+            return getRandomGameNumber(count);
+        }
+    }
+    private  static String getGameNumberString(int count) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < count ; i++) {
+            list.add(Integer.toString(getRandomNumber()));
+        }
+        return String.join("", list);
+    }
+
+    private static int getRandomNumber() {
+        Random random = new Random();
+        return (random.nextInt(MAX_NUMBER) + MIN_NUMBER);
+    }
+}
